3bf4a4feecf5e3bc5807c4090b3b260e8147a15f
Ludes Developer <ludes.developer@gmail.com>
2022-08-05 23:23:21 +0700

    Update support-app.sh

--- support-app.sh
+++ support-app.sh
@@ -1,1 +1,12 @@
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+wget https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_x86_64.tar.gz
+tar -xvf k9s_Linux_x86_64.tar.gz 
+chmod u+x k9s
+sudo mv k9s /usr/local/bin/
